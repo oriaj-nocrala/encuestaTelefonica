4969639304ff756a60a03456ab11b7e8f1f53895
jairobrecha <jairo.alarcon@brechadigital.cl>
2022-08-30 21:43:28 -0400

    Sidebar Toggle
    Arreglado el toggle del sidebar y el tamaño del <router-outlet>
    Agregadas las entradas de la sidebar con la interfaz SidebarRoutes

--- src/app/aplicacion/shared/sidebar/sidebar.component.ts
+++ src/app/aplicacion/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
+export interface SidebarRoutes{
+  routerLink:string,
+  name:string,
+  icon:string
+}
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -7,6 +13,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  rutas: SidebarRoutes[] = [
+    {
+      routerLink:'reporte',
+      name:'Reporte',
+      icon:'assesment'
+  },
+  {
+    routerLink:'asignar',
+    name:'Asignar',
+    icon:'assignment'
+  },
+  {
+    routerLink:'usuarios',
+    name:'Usuarios',
+    icon:'person'
+  },
+  ]
   constructor() { }
 
   ngOnInit(): void {
