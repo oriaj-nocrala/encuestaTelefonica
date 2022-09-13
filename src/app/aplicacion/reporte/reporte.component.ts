e5e3da4d3ec2739893755bc1b737be69643a4901
jairobrecha <jairo.alarcon@brechadigital.cl>
2022-09-13 17:56:32 -0300

    commit de dia martes zzzzz

--- src/app/aplicacion/reporte/reporte.component.ts
+++ src/app/aplicacion/reporte/reporte.component.ts
@@ -1,6 +1,9 @@
 import { DataService } from './../shared/servicios/data.service';
-import { Component} from '@angular/core';
-import { ChartConfiguration } from 'chart.js';
+import { Component, ViewChild} from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
+import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'app-reporte',
@@ -10,14 +13,25 @@ import { ChartConfiguration } from 'chart.js';
 
 export class ReporteComponent {
 
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
   progress:number = 45;
   data:any[] = [];
+  fecha:Date = new Date();
+  fechas: Date[] = [];
+  
 
   constructor( private dataService: DataService) { }
-
+  
+  
   ngOnInit(): void {
-    this.dataService.getDatosRespuestas().subscribe({next: data=> this.data= data,error: err=>console.log});
+    this.dataService.getDatosRespuestas().subscribe({next: data=> this.data=data,error: err=>console.log});
     console.log(this.data);
+    for(let i = 1; i <= 7; i++){
+      this.fechas.push( new Date(new Date().setDate(new Date().getDate()-i) ) ); //El día de ayer.
+    }
+    for(let i = 0; i < 7; i++)
+      console.log(this.fechas[i]);
   }
 
   procesarDatos(data:any[]){
@@ -26,9 +40,9 @@ export class ReporteComponent {
 
 
   public barChartLegend = true;
-  public barChartPlugins = [];
-
-  public barChartData: ChartConfiguration<'bar'>['data'] = {
+  public barChartPlugins = [DataLabelsPlugin];
+  public barChartType: ChartType = 'bar';
+  public barChartData: ChartData<'bar'> = {
     labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ], //Últimos 5 días
     datasets: [
       { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' }, //El nombre del analista en el label y en la data separada por días
@@ -36,7 +50,42 @@ export class ReporteComponent {
     ]
   };
 
-  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
-    responsive: false,
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    scales:{
+      x:{},
+      y:{min:10}
+    },
+    plugins: {
+      legend: {
+        display:true,
+      },
+      datalabels: {
+        anchor:'end',
+        align:'end'
+      }
+    }
   };
+
+  public randomize(): void {
+    // Only Change 3 values
+    this.barChartData.datasets[0].data = [
+      Math.round(Math.random() * 100),
+      59,
+      80,
+      Math.round(Math.random() * 100),
+      56,
+      Math.round(Math.random() * 100),
+      40 ];
+
+    this.chart?.update();
+  }
+  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    // console.log(event, active);
+  }
+
 }
