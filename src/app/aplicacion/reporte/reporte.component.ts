294b9c9d7bdc5bc5f4868df53e861a8dec2989ad
SebaBrecha <sebastian.hernandez@brechadigital.cl>
2022-09-20 12:07:56 -0300

    wena jairo

--- src/app/aplicacion/reporte/reporte.component.ts
+++ src/app/aplicacion/reporte/reporte.component.ts
@@ -1,36 +1,202 @@
-import { DataService } from './../shared/servicios/data.service';
-import { Component, OnInit, ViewChild} from '@angular/core';
-
-import {Analista, Respuesta, RespuestaCruzada, RespuestaXAnalista} from '../shared/interfaces/encuesta.interface'
-
-
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { DataService } from '../shared/servicios/data.service';
+import { HttpClient } from '@angular/common/http';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { Usuario } from '../shared/modelos/usuario';
+import { DataSource } from '@angular/cdk/collections';
+import { BehaviorSubject, fromEvent, merge, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-reporte',
   templateUrl: './reporte.component.html',
-  styleUrls: ['./reporte.component.scss']
+  styleUrls: ['./reporte.component.scss'],
 })
-
 export class ReporteComponent implements OnInit {
+  displayedColumns = [
+    'rut',
+    'nombre',
+    'apellido',
+    'correo',
+    'user',
+    'pass',
+    'telefono',
+    'actions',
+  ];
+  exampleDatabase!: DataService | null;
+  dataSource!: ExampleDataSource | null;
+  index!: number;
+  rut!: number;
 
-  respuestas:Respuesta[] = [];
+  constructor(
+    public httpClient: HttpClient,
+    public dialog: MatDialog,
+    public dataService: DataService
+  ) {}
 
-  constructor( private dataService: DataService) { }
-  
-  
-  ngOnInit(): void {
-    
-    this.dataService.getDatosRespuestas().subscribe( data => {
-      this.respuestas = data;
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort!: MatSort;
+  @ViewChild('filter', { static: true }) filter!: ElementRef;
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  refresh() {
+    this.loadData();
+  }
+
+  private refreshTable() {
+    // Refreshing table using paginator
+    // Thanks yeager-j for tips
+    // https://github.com/marinantonio/angular-mat-table-crud/issues/12
+    this.paginator._changePageSize(this.paginator.pageSize);
+  }
+
+  /*   // If you don't need a filter or a pagination this can be simplified, you just use code from else block
+    // OLD METHOD:
+    // if there's a paginator active we're using it for refresh
+    if (this.dataSource._paginator.hasNextPage()) {
+      this.dataSource._paginator.nextPage();
+      this.dataSource._paginator.previousPage();
+      // in case we're on last page this if will tick
+    } else if (this.dataSource._paginator.hasPreviousPage()) {
+      this.dataSource._paginator.previousPage();
+      this.dataSource._paginator.nextPage();
+      // in all other cases including active filter we do it like this
+    } else {
+      this.dataSource.filter = '';
+      this.dataSource.filter = this.filter.nativeElement.value;
+    }*/
+
+  public loadData() {
+    this.exampleDatabase = new DataService(this.httpClient);
+    this.dataSource = new ExampleDataSource(
+      this.exampleDatabase,
+      this.paginator,
+      this.sort
+    );
+    fromEvent(this.filter.nativeElement, 'keyup')
+      // .debounceTime(150)
+      // .distinctUntilChanged()
+      .subscribe(() => {
+        if (!this.dataSource) {
+          return;
+        }
+        this.dataSource.filter = this.filter.nativeElement.value;
+      });
+  }
+}
+
+export class ExampleDataSource extends DataSource<Usuario> {
+  _filterChange = new BehaviorSubject('');
+
+  get filter(): string {
+    return this._filterChange.value;
+  }
+
+  set filter(filter: string) {
+    this._filterChange.next(filter);
+  }
+
+  filteredData: Usuario[] = [];
+  renderedData: Usuario[] = [];
+
+  constructor(
+    public _exampleDatabase: DataService,
+    public _paginator: MatPaginator,
+    public _sort: MatSort
+  ) {
+    super();
+    // Reset to the first page when the user changes the filter.
+    this._filterChange.subscribe(() => (this._paginator.pageIndex = 0));
+  }
+
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<Usuario[]> {
+    this._sort = new MatSort();
+    // Listen for any changes in the base data, sorting, filtering, or pagination
+    const displayDataChanges = [
+      this._exampleDatabase.dataChange,
+      this._sort.sortChange,
+      this._filterChange,
+      this._paginator.page,
+    ];
+
+    this._exampleDatabase.getAllIssues();
+
+    return merge(...displayDataChanges).pipe(
+      map(() => {
+        // Filter data
+        this.filteredData = this._exampleDatabase.data
+          .slice()
+          .filter((usuario: Usuario) => {
+            const searchStr = (
+              usuario.rut +
+              usuario.nombre +
+              usuario.correo +
+              usuario.apellido
+            ).toLowerCase();
+            return searchStr.indexOf(this.filter.toLowerCase()) !== -1;
+          });
+
+        // Sort filtered data
+        const sortedData = this.sortData
+          ? this.sortData(this.filteredData.slice())
+          : [];
+
+        // Grab the page's slice of the filtered sorted data.
+        const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
+        this.renderedData = sortedData.splice(
+          startIndex,
+          this._paginator.pageSize
+        );
+        return this.renderedData;
+      })
+    );
+  }
+
+  disconnect() {}
+
+  /** Returns a sorted copy of the database data. */
+  sortData(data: Usuario[]): Usuario[] {
+    if (!this._sort.active || this._sort.direction === '') {
+      return data;
+    }
+
+    return data.sort((a, b) => {
+      let propertyA: number | string = '';
+      let propertyB: number | string = '';
+
+      switch (this._sort.active) {
+        case 'rut':
+          [propertyA, propertyB] = [a.rut, b.rut];
+          break;
+        case 'nombre':
+          [propertyA, propertyB] = [a.nombre, b.nombre];
+          break;
+        case 'apellido':
+          [propertyA, propertyB] = [a.apellido, b.apellido];
+          break;
+        case 'correo':
+          [propertyA, propertyB] = [a.correo, b.correo];
+          break;
+        case 'user':
+          [propertyA, propertyB] = [a.user, b.user];
+          break;
+        case 'telefono':
+          [propertyA, propertyB] = [a.telefono, b.telefono];
+          break;
+      }
+
+      const valueA = isNaN(+propertyA) ? propertyA : +propertyA;
+      const valueB = isNaN(+propertyB) ? propertyB : +propertyB;
+
+      return (
+        (valueA < valueB ? -1 : 1) * (this._sort.direction === 'asc' ? 1 : -1)
+      );
     });
-  }
-
-  
-  procesarDatos(data:any[]){
-    
-  }
-  
-  clickBoton(){
-    
   }
 }
--- src/app/aplicacion/shared/servicios/data.service.ts
+++ src/app/aplicacion/shared/servicios/data.service.ts
@@ -1,8 +1,8 @@
-import {Injectable} from '@angular/core';
-import {BehaviorSubject, Observable} from 'rxjs';
-import {Usuario} from '../modelos/usuario';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Usuario } from '../modelos/usuario';
 import { Empresa } from '../modelos/empresa';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Analista, RespuestaXAnalista } from '../interfaces/encuesta.interface';
 import { environment } from 'src/environments/environment';
 
@@ -16,7 +16,7 @@ export class DataService {
   // Temporarily stores data from dialogs
   dialogData: any;
 
-  constructor (private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {}
 
   get data(): Usuario[] {
     return this.dataChange.value;
@@ -28,18 +28,20 @@ export class DataService {
 
   /** CRUD METHODS */
   getAllIssues(): void {
-    this.httpClient.get<Usuario[]>(`${this.API_URL}/getDatosUsuarios`).subscribe({
-      next:data => {
-        this.dataChange.next(data);
-      },
-      error:(error: HttpErrorResponse) => {
-      console.log (error.name + ' ' + error.message);
-      }
-    });
+    this.httpClient
+      .get<Usuario[]>(`${this.API_URL}/getDatosUsuarios`)
+      .subscribe({
+        next: (data) => {
+          this.dataChange.next(data);
+        },
+        error: (error: HttpErrorResponse) => {
+          console.log(error.name + ' ' + error.message);
+        },
+      });
   }
 
   getDatosUsuarios(): Observable<Usuario[]> {
-    return this.httpClient.get<Usuario[]>(`${this.API_URL}/getDatosUsuarios`)
+    return this.httpClient.get<Usuario[]>(`${this.API_URL}/getDatosUsuarios`);
   }
 
   // DEMO ONLY, you can find working methods below
@@ -47,32 +49,49 @@ export class DataService {
   //   this.dialogData = usuario;
   // }
 
-
   // UPDATE, PUT METHOD
   addIssue(usuario: Usuario): void {
-    this.httpClient.post(`${this.API_URL}/agregarUsuario?rut=${usuario.rut}&nombre=${usuario.nombre}&apellido=${usuario.apellido}&correo=${usuario.correo}&telefono=${usuario.telefono}&user=${usuario.user}&pass=${usuario.pass}`, usuario).subscribe(data => {
-        this.dialogData = usuario;
-        // this.toasterService.showToaster('Successfully edited', 3000);
-        console.log("Bakán.");
-      },
-      (err: HttpErrorResponse) => {
-        // this.toasterService.showToaster('Error occurred. Details: ' + err.name + ' ' + err.message, 8000);
-        console.log("De perro.");
-      }
-    );
+    this.httpClient
+      .post(
+        `${this.API_URL}/agregarUsuario?rut=${usuario.rut}&nombre=${usuario.nombre}&apellido=${usuario.apellido}&correo=${usuario.correo}&telefono=${usuario.telefono}&user=${usuario.user}&pass=${usuario.pass}`,
+        usuario
+      )
+      .subscribe(
+        (data) => {
+          this.dialogData = usuario;
+          // this.toasterService.showToaster('Successfully edited', 3000);
+          console.log('Bakán.');
+        },
+        (err: HttpErrorResponse) => {
+          // this.toasterService.showToaster('Error occurred. Details: ' + err.name + ' ' + err.message, 8000);
+          console.log('De perro.');
+        }
+      );
   }
 
-  addRespuestas(usuario_id: string, padron_id:string,preguntas:any[], respuestas:any[], otro:string='', correo:string=''){
-    this.httpClient.post(`${this.API_URL}/agregarRespuesta?analista=${usuario_id}&padron=${padron_id}&pregunta1=${preguntas[0]._id}&pregunta2=${preguntas[1]._id}&pregunta3=${preguntas[2]._id}&pregunta4=${preguntas[3]._id}&respuesta1=${respuestas[0]}&respuesta2=${respuestas[1]}&respuesta3=${respuestas[2]}&respuesta4=${respuestas[3]}&otro=${otro}&correo=${correo}`,'a').subscribe(data => {
-      console.log("Bakán");
-    }),
-    (err: HttpErrorResponse) =>{
-      console.log("De perro.");
-    }
+  addRespuestas(
+    usuario_id: string,
+    padron_id: string,
+    preguntas: any[],
+    respuestas: any[],
+    otro: string = '',
+    correo: string = ''
+  ) {
+    this.httpClient
+      .post(
+        `${this.API_URL}/agregarRespuesta?analista=${usuario_id}&padron=${padron_id}&pregunta1=${preguntas[0]._id}&pregunta2=${preguntas[1]._id}&pregunta3=${preguntas[2]._id}&pregunta4=${preguntas[3]._id}&respuesta1=${respuestas[0]}&respuesta2=${respuestas[1]}&respuesta3=${respuestas[2]}&respuesta4=${respuestas[3]}&otro=${otro}&correo=${correo}`,
+        'a'
+      )
+      .subscribe((data) => {
+        console.log('Bakán');
+      }),
+      (err: HttpErrorResponse) => {
+        console.log('De perro.');
+      };
   }
 
   //get
-  getDatosPreguntas():Observable<any[]>{
+  getDatosPreguntas(): Observable<any[]> {
     return this.httpClient.get<any[]>(`${this.API_URL}/getDatosPreguntas`);
   }
   getAllEmpresas(): Observable<Empresa[]> {
@@ -84,40 +103,50 @@ export class DataService {
     return this.httpClient.get<String[]>(`${this.API_URL}/getDatosBodegas`);
   }
 
-  getDatosAnalistas(): Observable<Analista[]>{
+  getDatosAnalistas(): Observable<Analista[]> {
     return this.httpClient.get<Analista[]>(`${this.API_URL}/getDatosAnalistas`);
   }
 
-  getCountRespuestasPorAnalista(): Observable<RespuestaXAnalista[]>{
-    return this.httpClient.get<RespuestaXAnalista[]>(`${this.API_URL}/getCountRepuestasPorAnalista`);
-
+  getCountRespuestasPorAnalista(): Observable<RespuestaXAnalista[]> {
+    return this.httpClient.get<RespuestaXAnalista[]>(
+      `${this.API_URL}/getCountRepuestasPorAnalista`
+    );
   }
 
-  getDatosAsignaciones(): Observable<any[]>{
+  getDatosAsignaciones(): Observable<any[]> {
     return this.httpClient.get<any[]>(`${this.API_URL}/getDatosAsignaciones`);
   }
 
-  getDatosPadron(analista: any):Observable<any[]>{
-    return this.httpClient.get<any[]>(`${this.API_URL}/getDatosPadronPorAnalista?analista=${analista}`);
+  getDatosPadron(analista: any): Observable<any[]> {
+    return this.httpClient.get<any[]>(
+      `${this.API_URL}/getDatosPadronPorAnalista?analista=${analista}`
+    );
   }
 
-  getDatosRespuestas():Observable<any[]>{
+  getDatosRespuestas(): Observable<any[]> {
     return this.httpClient.get<any[]>(`${this.API_URL}/getDatosRespuestas`);
   }
 
-  getPosiblesRespuestas():Observable<any[]>{
+  getPosiblesRespuestas(): Observable<any[]> {
     return this.httpClient.get<any[]>(`${this.API_URL}/getPosiblesRespuestas`);
   }
 
-  updateIssue (usuario: Usuario): void {
-    this.httpClient.put(`${this.API_URL}/actualizarUsuario?_id=${usuario._id}&rut=${usuario.rut}&nombre=${usuario.nombre}&apellido=${usuario.apellido}&correo=${usuario.correo}&telefono=${usuario.telefono}&user=${usuario.user}&pass=${usuario.pass}`, null).subscribe(data => {
-      this.dialogData = usuario;
-      console.log("Bakán.");
-    },
-    (err: HttpErrorResponse) => {
-      // this.toasterService.showToaster('Error occurred. Details: ' + err.name + ' ' + err.message, 8000);
-      console.log("De perro.");
-    });
+  updateIssue(usuario: Usuario): void {
+    this.httpClient
+      .put(
+        `${this.API_URL}/actualizarUsuario?_id=${usuario._id}&rut=${usuario.rut}&nombre=${usuario.nombre}&apellido=${usuario.apellido}&correo=${usuario.correo}&telefono=${usuario.telefono}&user=${usuario.user}&pass=${usuario.pass}`,
+        null
+      )
+      .subscribe(
+        (data) => {
+          this.dialogData = usuario;
+          console.log('Bakán.');
+        },
+        (err: HttpErrorResponse) => {
+          // this.toasterService.showToaster('Error occurred. Details: ' + err.name + ' ' + err.message, 8000);
+          console.log('De perro.');
+        }
+      );
   }
 
   // deleteIssue (rut: number): void {
@@ -125,27 +154,35 @@ export class DataService {
   // }
 
   deleteIssue(rut: number): void {
-    this.httpClient.delete(`${this.API_URL}/eliminarUsuarioPorRut?rut=${rut}`).subscribe({
-      next: (data) => {
-      console.log(data);
-      console.log("Bakán.");
-      },
-      error: (err: HttpErrorResponse) => {
-        console.log("De perro.");      }
+    this.httpClient
+      .delete(`${this.API_URL}/eliminarUsuarioPorRut?rut=${rut}`)
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+          console.log('Bakán.');
+        },
+        error: (err: HttpErrorResponse) => {
+          console.log('De perro.');
+        },
       });
   }
 
-  putAsignacion(analista:string, bodega:string){
-    this.httpClient.put(`${this.API_URL}/putAsignacion?analista=${analista}&bodega=${bodega}`, null).subscribe({
-      next: (data) => {
-      console.log(data);
-      console.log("Bakán.");
-      },
-      error: (err: HttpErrorResponse) => {
-        console.log("De perro.");      }
+  putAsignacion(analista: string, bodega: string) {
+    this.httpClient
+      .put(
+        `${this.API_URL}/putAsignacion?analista=${analista}&bodega=${bodega}`,
+        null
+      )
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+          console.log('Bakán.');
+        },
+        error: (err: HttpErrorResponse) => {
+          console.log('De perro.');
+        },
       });
   }
-
 
   //NUEVOS METODOS
 
@@ -153,17 +190,14 @@ export class DataService {
     this.dialogData = usuario;
   }
 
-  updateUsuario(usuario: Usuario): void{
-    this.dialogData = usuario
+  updateUsuario(usuario: Usuario): void {
+    this.dialogData = usuario;
   }
 
-  deleteUsuario (rut: number): void {
+  deleteUsuario(rut: number): void {
     console.log(rut);
   }
-
 }
-
-
 
 /* REAL LIFE CRUD Methods I've used in my projects. ToasterService uses Material Toasts for displaying messages:
 
@@ -202,7 +236,3 @@ export class DataService {
     );
   }
 */
-
-
-
-
